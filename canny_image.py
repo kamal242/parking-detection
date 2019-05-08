4c211715a528fc296ad69bb3b8103eea5c85563e
abhisheksinha582 <47332311+abhisheksinha582@users.noreply.github.com>
2019-05-08 22:03:07 +0530

    Add files via upload

---
+++ canny_image.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('IMG_20180326_003246.jpg', 0)
+edge = cv.Canny(img, 100, 200)
+img_gauss = cv.GaussianBlur(edge,(5,5), 0)
+
+plt.subplot(121),plt.imshow(img, cmap='gray')
+plt.title('Original image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edge, cmap='gray')
+plt.title('Edge image'), plt.xticks([]), plt.yticks([])
+
+plt.imshow(img_gauss, cmap='gray')
+plt.title('Gauss image'), plt.xticks([]), plt.yticks([])
+
+
+
+plt.show()
